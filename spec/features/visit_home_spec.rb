b1fd303dec5999a23a24d4612e40c3905198a938
Le Si Bich <le.si.bich@framgia.com>
2018-11-09 09:22:10 +0700

    feature test

---
+++ spec/features/visit_home_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature 'VisitHomes', type: :feature do
+  let!(:categories) { create_list(:category, 3) }
+  let!(:topic) { create(:topic, category: categories.first, name: 'Topic 1') }
+
+  scenario 'user can visit the category page' do
+    visit root_path
+
+    expect(page).to have_css('.category-list-item', count: 3)
+    expect(page).to have_content(categories.first.name)
+  end
+
+  scenario 'user can see the last topic of each categories' do
+    visit root_path
+
+    expect(page).to have_content('Topic 1')
+    expect(page).to have_content(topic.creator.username)
+  end
+end
