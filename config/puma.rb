ffeb7c45b0e064e6940d9a509d8a23270ea5b2f7
Le Si Bich <le.si.bich@framgia.com>
2018-11-22 09:04:16 +0700

    docker production

--- config/puma.rb
+++ config/puma.rb
@@ -1,21 +1,16 @@
+workers ENV.fetch('RAILS_WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
-preload_app!
-
 app_dir = File.expand_path('..', __dir__)
 
+preload_app!
+
 environment ENV.fetch('RAILS_ENV') { 'development' }
-
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
-
-activate_control_app
+port ENV.fetch('RAILS_PORT') { 3000 }
 
 on_worker_boot do
-  require 'activerecord'
+  require 'active_record'
   begin
     ActiveRecord::Base.connection.disconnect!
   rescue StandardError
