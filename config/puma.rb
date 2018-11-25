ee7cfd547f569e21a76705bb1f991f6be5375f5b
_MoonLight_ <moonlight8978@gmail.com>
2018-11-25 18:18:31 +0700

    remove app_dir

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,6 @@ workers ENV.fetch('RAILS_WEB_CONCURRENCY') { 2 }
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
-app_dir = File.expand_path('..', __dir__)
-
 preload_app!
 
 environment ENV.fetch('RAILS_ENV') { 'development' }
